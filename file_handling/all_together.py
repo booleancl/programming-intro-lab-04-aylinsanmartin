efb070ac382d97dbb4c78b2a010eca84ec23ae33
aylin <a.sanlazo@gmail.com>
2022-09-20 10:22:14 -0300

    white

---
+++ file_handling/all_together.py
@@ -0,0 +1,27 @@
+
+print("Bienvenidos al programa")
+user_input = ""
+
+while user_input != "exit":
+    print("#################")
+    print("Ingresa una opcion")
+    print("1", "agregar contenido")
+    print("2", "leer contenido")
+    print("exit", "para salir")
+    print("##################")
+
+    user_input = input()
+
+    if user_input == "1":
+        file =open("demo_two.txt","a")
+        user_content = input("Ingresa el contendido\n")
+        file_write(user_content + "\n")
+        file.close()
+    elif user_input == "2":
+        file = open("demo_two.txt", "r")
+        for line in file:
+            print(line)
+    else:
+        print("opcion incorrecta")
+
+print("chau chau") 
---
+++ fundamentals/15_while_recap.py
@@ -0,0 +1,8 @@
+# El while pertenece a las estructuras de control que repiten sentencias 
+# similar a for, pero ejecuta sus sentencias mientras la condicion son : 
+
+i = 5
+
+while i < 6:
+    print(i)
+    i += 1
